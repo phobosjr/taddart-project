c9e42d850feaaae5b59a4f09acd7169b81850a39
phobosjr <sehaki.menad@gmail.com>
2021-03-24 23:01:48 +0100

    td-front add lang header.

--- taddart-front/components/Header.vue
+++ taddart-front/components/Header.vue
@@ -3,27 +3,37 @@
        :style="{backgroundImage: 'url(http://localhost:1337'+header.background_image[0].url+')'}">
     <nav class="Header__navbar navbar navbar-light position-fixed td-transition">
       <nuxt-link class="Header__navbar__logo navbar-brand" to="/">
-        <img :src="'http://localhost:1337'+header.logo_image.url" width="30" height="30" class="d-inline-block align-top td-transition" alt="">
+        <img :src="'http://localhost:1337'+header.logo_image.url" width="30" height="30"
+             class="Header__navbar__logo__img d-inline-block align-top td-transition" alt="">
       </nuxt-link>
-    </nav>
 
+      <div class="Header__navbar__lang">
+        <span>
+          <img class="Header__navbar__lang__img Header__navbar__lang__img_current" src="~/assets/images/header/kb-lang-32-32.png" alt="KB">
+          </span>
+        <span>
+          <img class="Header__navbar__lang__img" src="~/assets/images/header/fr-lang.svg" alt="FR">
+        </span>
+      </div>
+    </nav>
   </div>
 </template>
 <script>
 import headerQuery from '@/apollo/queries/header/header.gql'
-export default {
-props: {
-  smallHeader: {type: Boolean, required: false, default: false}
-},
 
-  data: ()=>{
+export default {
+  props: {
+    smallHeader: {type: Boolean, required: false, default: false}
+  },
+
+  data: () => {
     return {
       isScrolled: false
     }
-},
+  },
   apollo: {
     header: {
-      prefetch:true,
+      prefetch: true,
       query: headerQuery
     }
   },
@@ -34,7 +44,7 @@ props: {
         : (this.isScrolled = true);
     },
   },
-  beforeMount () {
+  beforeMount() {
     window.addEventListener('scroll', this.handleScroll);
   },
   beforeDestroy() {
@@ -55,30 +65,49 @@ props: {
   &.scrolled {
     height: 13em;
 
-    nav {
+    .Header__navbar {
       height: 60px;
       background-color: rgba(96, 168, 239, 0.85) !important;
-      img {
-        width: 50px;
-        height: 50px;
+
+      .Header__navbar__logo {
+        .Header__navbar__logo__img {
+          width: 50px;
+          height: 50px;
+        }
       }
     }
   }
 
-  nav {
+  .Header__navbar {
     height: 120px;
     padding: 0 204px;
     background-color: rgba(96, 168, 239, 0.38) !important;
     width: 100%;
 
-    img {
-      width: 100px;
-      height: 100px;
+    .Header__navbar__logo {
+      .Header__navbar__logo__img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+
+    .Header__navbar__lang {
+      .Header__navbar__lang__img {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        opacity: .25;
+        &.Header__navbar__lang__img_current {
+          opacity: 1;
+          cursor: default;
+        }
+      }
     }
   }
 }
+
 .td-transition {
-  transition: all 0.25s cubic-bezier(.6,.04,.98,.34);
+  transition: all 0.25s cubic-bezier(.6, .04, .98, .34);
 }
 
 .small-height {
