c2c18af908c5b43b8074ae9f7ac23877b9966b76
phobosjr <sehaki.menad@gmail.com>
2021-03-24 22:22:29 +0100

    td-refacto rename front package and back package.

---
+++ taddart-front/layouts/layoutWithSmallHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Default-layout">
+    <Header class="Default-layout__header" :small-header="true"></Header>
+    <div class="Default-layout__main">
+        <nuxt/>
+    </div>
+    <Footer class="Default-layout__footer"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  components: {Header},
+}
+</script>
+
+<style lang="scss" scoped>
+.Default-layout {
+
+  .Default-layout__main {
+    height: 100vh;
+  }
+}
+</style>
---
+++ taddart-front/pages/contact/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div :class="['Contact', 'position-relative', 'p-5']" :style="{backgroundImage: 'url(http://localhost:1337'+contact.background_image.url+')'}">
+
+    <div class="Contact__title d-flex justify-content-center">
+      <h1>Nous contacter</h1>
+    </div>
+
+    <form class="Contact__form m-auto p-3">
+      <v-text-field
+        v-model="name"
+        :label="contact.name_label['kb']"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :label="contact.email_label['kb']"
+        required
+      ></v-text-field>
+
+      <v-textarea
+        v-model="message"
+        :counter="120"
+        :label="contact.message_label['kb']"
+        value=""
+      ></v-textarea>
+      <div class="Contact__form__btn d-flex justify-content-end">
+        <v-btn
+          elevation="2"
+          large
+          class=""
+        >
+          {{contact.button_label['kb']}}
+        </v-btn>
+      </div>
+    </form>
+
+  </div>
+
+</template>
+
+<script>
+import contactQuery from '@/apollo/queries/contact/contact.gql'
+export default {
+  name: "contact",
+  layout: 'layoutWithSmallHeader',
+
+  apollo: {
+    contact: {
+      prefetch: true,
+      query: contactQuery
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Contact {
+  height: 100vh;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  .Content__title {
+    margin: 20px;
+  }
+
+  .Contact__form {
+    height: 50vh;
+    width: 60%;
+    background-color: #ffffffad;
+    right: 0;
+    left: 0;
+    top: 53px;
+
+    .Contact__form__btn {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
