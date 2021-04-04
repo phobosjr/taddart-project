f161ba3b77843e265ed866a991c5976d160875ac
phobosjr <sehaki.menad@gmail.com>
2021-04-04 11:41:28 +0200

    wip header responsive add large format defaultImage.js

--- taddart-front/plugins/filters/strapiImage/defaultImage.js
+++ taddart-front/plugins/filters/strapiImage/defaultImage.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 
 function getPictureUrl (formats) {
+  if (formats?.large){
+    return formats?.large.url;
+  }
   return formats?.medium ? formats.medium.url : formats?.small?.url;
 }
 
