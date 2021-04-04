fce564214c61954914c04019abcd50948e505e78
phobosjr <sehaki.menad@gmail.com>
2021-04-04 17:44:33 +0200

    td-front refactoring contact and map page.

--- taddart-front/lang/kb-KB.js
+++ taddart-front/lang/kb-KB.js
@@ -1,11 +1,17 @@
 export default {
   home_title: 'Agejdan',
   gallery_title: 'Tagalrit',
-  contact_title: 'Anermis',
   map_title: 'Tkarḍa n taddart',
   all_albums_title: 'Akk',
   last_article_title: 'Imagraden ineggura',
   read_article_label: 'Ɣeṛ amagrad-a',
   article_author_label: 'Sγur: ',
+  contact_title: 'Anermis',
+  contact_form_name: 'Isem',
+  contact_form_email: 'Lmay',
+  contact_form_message: 'Azen',
+  contact_form_submit_btn: '',
+  contact_form_alert_success_message: '',
+  contact_form_alert_failed_message: ''
 
 }
