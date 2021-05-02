717660dd749e8fc1a9b7387ee61a3f491afba970
phobosjr <sehaki.menad@gmail.com>
2021-05-02 20:09:47 +0200

    fix url access token config

--- taddart-front/pages/auth/google/callback/index.vue
+++ taddart-front/pages/auth/google/callback/index.vue
@@ -6,13 +6,13 @@
 export default {
   name: "index",
   layout: 'layoutWithSmallHeader',
-  async middleware({redirect, $cookies, $axios, route}) {
+  async middleware({redirect, $cookies, $axios, route, $config}) {
     const lastPath = $cookies.get('last_path');
     $cookies.remove('last_path');
     const acces_token_regex = new RegExp('(?<=access_token=).*?(?=&)');
     const [access_token] = acces_token_regex.exec(route.fullPath);
     console.log(access_token);
-    const response = await $axios.$get(`${this.$config.strapiBackendUrl}/auth/google/callback?access_token=${access_token}`);
+    const response = await $axios.$get(`${$config.strapiBackendUrl}/auth/google/callback?access_token=${access_token}`);
     $cookies.set('strapi_jwt', response.jwt);
     $cookies.set('user_access_token', access_token);
     return redirect(lastPath || '/');
