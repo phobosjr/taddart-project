58d7f46fac45390e54156408296801e783ca418b
phobosjr <sehaki.menad@gmail.com>
2021-05-10 23:36:48 +0200

    wip fix: facebook authentication provider

--- taddart-front/pages/auth/facebook/callback/index.vue
+++ taddart-front/pages/auth/facebook/callback/index.vue
@@ -6,16 +6,16 @@
 export default {
   name: "index",
   layout: 'layoutWithSmallHeader',
-  async middleware({redirect, $cookies, $axios, route}) {
-    const lastPath = $cookies.get('last_path');
-    $cookies.remove('last_path');
-    const acces_token_regex = new RegExp('(?<=access_token=).*?(?=&)');
-    const [access_token] = acces_token_regex.exec(route.fullPath);
-    console.log(access_token);
-    const response = await $axios.$get(`/auth/google/callback?access_token=${access_token}`);
-    $cookies.set('strapi_jwt', response.jwt);
-    $cookies.set('user_access_token', access_token);
-    return redirect(lastPath || '/');
+  async created() {
+    if (process.client) {
+      const lastPath = this.$cookies.get('last_path');
+      this.$cookies.remove('last_path');
+      const acces_token_regex = new RegExp('(?<=access_token=).*?(?=&)');
+      const [access_token] = acces_token_regex.exec(this.$route.fullPath);
+      this.$cookies.set('user_access_token', access_token);
+      await this.$store.dispatch("user/fetchUser");
+      window.location = lastPath || '/';
+    }
   }
 }
 </script>
