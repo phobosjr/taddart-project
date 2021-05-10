ea584f9c2318b3fc5dd71dd8777b99ccdbae2f8d
phobosjr <sehaki.menad@gmail.com>
2021-05-10 23:21:57 +0200

    wip fix: bypass strapi jwt move to store

--- taddart-front/pages/auth/google/callback/index.vue
+++ taddart-front/pages/auth/google/callback/index.vue
@@ -6,13 +6,14 @@
 export default {
   name: "index",
   layout: 'layoutWithSmallHeader',
-  middleware({$cookies, route}) {
+  async created() {
     if (process.client) {
-      const lastPath = $cookies.get('last_path');
-      $cookies.remove('last_path');
+      const lastPath = this.$cookies.get('last_path');
+      this.$cookies.remove('last_path');
       const acces_token_regex = new RegExp('(?<=access_token=).*?(?=&)');
-      const [access_token] = acces_token_regex.exec(route.fullPath);
-      $cookies.set('user_access_token', access_token);
+      const [access_token] = acces_token_regex.exec(this.$route.fullPath);
+      this.$cookies.set('user_access_token', access_token);
+      await this.$store.dispatch("user/fetchUser");
       window.location = lastPath || '/';
     }
   }
